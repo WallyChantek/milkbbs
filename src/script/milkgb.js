10f2fb9fe2be3825229f0a40d19889f70f778d36
Wally Chantek <wallybangerchantek@gmail.com>
2021-01-07 19:37:20 -0500

    Added in core formatting toolbar functionality (no actual formatting support yet)

--- src/script/milkgb.js
+++ src/script/milkgb.js
@@ -8,6 +8,27 @@ var milkgb = {};
 
 milkgb.initMainPage = function() {
     /*
+      Post formatting buttons
+    */
+    if (document.getElementById('milkgb-posting-form-formatting')) {
+        document.getElementById('milkgb-formatting-bold').addEventListener('click', function(event) {
+            milkgb.insertTag('milkgb-posting-form-comment', 'b');
+        });
+        document.getElementById('milkgb-formatting-italic').addEventListener('click', function(event) {
+            milkgb.insertTag('milkgb-posting-form-comment', 'i');
+        });
+        document.getElementById('milkgb-formatting-underline').addEventListener('click', function(event) {
+            milkgb.insertTag('milkgb-posting-form-comment', 'u');
+        });
+        document.getElementById('milkgb-formatting-color').addEventListener('change', function(event) {
+            if (this.value !== '') {
+                milkgb.insertTag('milkgb-posting-form-comment', 'color=' + this.value);
+                this.selectedIndex = 0;
+            }
+        });
+    }
+    
+    /*
        Make it so clicking a [Delete] button takes the user to the
        Entry Management section, and auto-populates the ID field.
     */
@@ -21,6 +42,24 @@ milkgb.initMainPage = function() {
             document.getElementById('milkgb-entry-management-entry-id-visual').value = entryId;
             document.getElementById('milkgb-entry-management-password').focus();
         });
+    }
+};
+
+milkgb.insertTag = function(textareaId, tag) {
+    var elem = document.getElementById(textareaId);
+    if (elem.selectionStart || elem.selectionStart == '0') {
+        var tagStart = '[' + tag + ']';
+        var tagEnd = '[/' + tag + ']';
+        var posStart = elem.selectionStart;
+        var posEnd = elem.selectionEnd + tagStart.length;
+        
+        console.log('s: ' + posStart);
+        console.log('e: ' + posEnd);
+        
+        var newStr = elem.value;
+        newStr = [newStr.slice(0, posStart), tagStart, newStr.slice(posStart)].join('');
+        newStr = [newStr.slice(0, posEnd), tagEnd, newStr.slice(posEnd)].join('');
+        elem.value = newStr;
     }
 };
 
